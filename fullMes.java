5ae670d196276dc91bd553f1420943a8766063fb
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-29 21:17:20 +0400

    Update fullMes.java

--- fullMes.java
+++ fullMes.java
@@ -1,12 +1,12 @@
-public class fullMes{
-  public static void main(String [] arg){
+public class fullMes {
+  public static void main(String[] arg) {
     int x = 20;
     int a = 10;
     int b = a + x;
     String message = "Hello how are you today";
-    
+
     String fullMessage = message + "\n" + "I'm doing fine x is " + x;
-    
-    System.out.println (fullMessage);
+
+    System.out.println(fullMessage);
   }
 }
